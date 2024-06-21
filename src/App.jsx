52e1f4646c2874a8571ba9b8566597295554f05e
JBKIM <drv98@naver.com>
2024-06-21 15:21:22 +0900

    프롭스로 title icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import "./App.css";
+import todoIcon from "./assets/direct-hit.png";
+import doingIcon from "./assets/glowing-star.png";
+import doneIcon from "./assets/check-mark-button.png";
+
 import TaskColumn from "./components/TaskColumn";
 import TaskForm from "./components/TaskForm";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <TaskForm />
       <main className="app_main">
-        <TaskColumn />
-        <TaskColumn />
-        <TaskColumn />
+        <TaskColumn title="할 일" icon={todoIcon} />
+        <TaskColumn title="진행중" icon={doingIcon} />
+        <TaskColumn title="완 료" icon={doneIcon} />
       </main>
     </div>
   );
--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,12 +1,11 @@
-import Todo from "../assets/direct-hit.png";
 import "./TaskColumn.css";
 
-export default function TaskColumn() {
+export default function TaskColumn({ title, icon }) {
   return (
     <section className="task_column">
       <h2 className="task_column_heading">
-        <img className="task_column_icon" src={Todo} alt="" />
-        할일
+        <img className="task_column_icon" src={icon} alt="" />
+        {title}
       </h2>
     </section>
   );
